48c16c4ebdce71fd6f2d61006369794205075cfa
Caleb Smith-Plummer <smithpcp@mail.uc.edu>
2022-10-13 00:29:56 -0400

    Revised Lobby manager

--- Module 6/Assets/Scripts/LobbyManager.cs
+++ Module 6/Assets/Scripts/LobbyManager.cs
@@ -15,15 +15,55 @@ public class LobbyManager : NetworkBehaviour
     public Button btnStart;
     public Player playerPrefab;
 
+    private NetworkList<PlayerInfo> allPlayers = new NetworkList<PlayerInfo>();
+    private List<LobbyPlayerPanel> playerPanels = new List<LobbyPlayerPanel>();
 
-    void Start()
-    {
+
+    public void Start() {
+        if (IsHost) {
+            AddPlayerToList(NetworkManager.LocalClientId);
+            RefreshPlayerPanels();
+        }
     }
-
 
     public override void OnNetworkSpawn() {
+        
+
+        if (IsHost) {
+            NetworkManager.Singleton.OnClientConnectedCallback += HostOnClientConnected;
+            //NetworkManager.Singleton.OnClientDisconnectCallback += HostOnClientDisconnected;
+        }
         base.OnNetworkSpawn();
+    }
+
+    private void HostOnClientConnected(ulong clientId) {
+        AddPlayerToList(clientId);
+        RefreshPlayerPanels();
+    }
+
+
+    private void AddPlayerToList(ulong clientId) {
+        allPlayers.Add(new PlayerInfo(clientId, Color.red));
+    }
+
+    private void AddPlayerPanel(PlayerInfo info) {
+        LobbyPlayerPanel newPanel = Instantiate(playerPanelPrefab);
+        newPanel.transform.SetParent(playerScrollContent.transform, false);
+        newPanel.SetName($"Player {info.clientId.ToString()}");
+        newPanel.SetColor(info.color);
+        playerPanels.Add(newPanel);
 
     }
 
+    private void RefreshPlayerPanels() {
+        foreach (LobbyPlayerPanel panel in playerPanels) {
+            Destroy(panel.gameObject);
+        }
+        playerPanels.Clear();
+
+        foreach (PlayerInfo pi in allPlayers) {
+            AddPlayerPanel(pi);
+        }
+    }
+    
 }
